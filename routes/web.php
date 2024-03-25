46e67151d416a44343262cd1b5d003a30591d467
newt <axelskam.123@outlook.es>
2024-03-25 09:03:17 -0500

    feat: :sparkles: set up web routes
    set up home, login, logout, register and profile routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name("home")->middleware("auth");
+
+Route::view('/login', 'auth.login')->name("login");
+Route::post('/login', [AuthenticatedSessionController::class, "store"])->name("login");
+Route::post('/logout', [AuthenticatedSessionController::class, "destroy"])->name("logout")->middleware("auth");
+
+Route::view('/register', 'auth.register')->name("register");
+Route::post('/register', [RegisteredUserController::class, "store"])->name("register");
+
+Route::view('/{user}', 'profile')->name("profile");
+Route::view('/{user}/settings', 'settings')->name("settings")->middleware("auth");
