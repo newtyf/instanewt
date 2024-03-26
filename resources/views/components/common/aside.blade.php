f63b5f72254100f7bb0e7a62c9023f7cfb3a7e10
newt <axelskam.123@outlook.es>
2024-03-26 09:53:49 -0500

    feat: :art: change home to feed route

--- resources/views/components/common/aside.blade.php
+++ resources/views/components/common/aside.blade.php
@@ -1,31 +1,38 @@
-<nav class="h-full p-3 border-r w-min border-neutral-700 hidden md:flex flex-col justify-between">
+<nav class="h-full p-3 border-r w-20 border-neutral-700 hidden md:flex flex-col justify-between">
     <ul class="flex flex-col items-center my-5">
         <li>
-            <a href="{{ route('home') }}"
+            <a href="{{ route('feed') }}"
                 class="text-white hover:bg-white hover:bg-opacity-15 hover:scale-105 rounded-md flex items-center justify-center p-2"
-                title="logo instanewt">
+                title="logo Instanewt">
                 <i class="bi-instagram text-xl"></i></a>
         </li>
     </ul>
-    <ul class="flex flex-col flex-1 items-center">
+    <ul class="flex flex-col flex-1 items-center space-y-2">
         <li>
-            <a href="{{ route('home') }}"
-                class="text-white hover:bg-white hover:bg-opacity-15 hover:scale-105 rounded-md flex items-center justify-center p-2 mb-2"
-                title="go to the home"><i class="text-2xl bi @if(request()->routeIs('home')) bi-house-door-fill @else bi-house-door @endif"></i></a>
+            <a href="{{ route('feed') }}"
+                class="text-white hover:bg-white hover:bg-opacity-15 hover:scale-105 rounded-md flex items-center justify-center p-2"
+                title="go to the feed"><i
+                    class="text-2xl bi @if (request()->routeIs('feed')) bi-house-door-fill @else bi-house-door @endif"></i></a>
         </li>
         <li>
-            <a href="{{ route('profile', ['user' =>  auth()->user()->username]) }}"
-                class="text-white hover:bg-white hover:bg-opacity-15 hover:scale-105 rounded-md flex items-center justify-center p-2 mb-2">
-                <div class="rounded-full overflow-hidden">
-                    <img src="https://newtyf.com/profile.webp"
-                        alt="" class="object-cover aspect-square">
+            <a href="{{ route('posts.create') }}"
+                class="text-white hover:bg-white hover:bg-opacity-15 hover:scale-105 rounded-md flex items-center justify-center p-2"
+                title="go to the feed"><i
+                    class="text-xl bi @if (request()->routeIs('posts.create')) bi-plus-square-fill @else bi-plus-square @endif"></i></a>
+        </li>
+        <li>
+            <a href="{{ route('account.profile', ['user' => auth()->user()->username]) }}"
+                class="text-white hover:bg-white hover:bg-opacity-15 hover:scale-105 rounded-md flex items-center justify-center">
+                <div class="rounded-full w-6 h-6 m-2 overflow-hidden">
+                    <img src="@if (!auth()->user()->photoUrl) /images/profile-photo.webp @else {{ auth()->user()->photoUrl }} @endif"
+                        alt="" class="w-full h-full object-cover">
                 </div>
             </a>
         </li>
     </ul>
     <ul class="flex flex-col items-center">
         <li>
-            <a href="{{ route('settings', ['user' =>  auth()->user()->username]) }}"
+            <a href="{{ route('account.settings') }}"
                 class="text-white hover:bg-white hover:bg-opacity-15 hover:scale-105 rounded-md flex items-center justify-center p-2"
                 title="go to the settings">
                 <i class="bi bi-gear-wide text-2xl"></i></a>
