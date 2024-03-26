f2101da95753ef76e126373b98f3485dffe67086
newt <axelskam.123@outlook.es>
2024-03-26 09:50:25 -0500

    feat: :sparkles: add custom 404 page not found

---
+++ resources/views/404.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app title="404 | Not found">
+  <section class="w-full mx-auto my-20 max-w-screen-sm text-center">
+    <h1 class="text-white text-2xl">404 | page not found</h1>
+  </section>
+</x-layouts.app>
