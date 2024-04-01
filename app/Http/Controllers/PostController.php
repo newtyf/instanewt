0f942748edfd4364cdf2aa67a285fce3fc38a95f
newt <axelskam.123@outlook.es>
2024-04-01 11:28:44 -0500

    feat: :sparkles: Add list only user following posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,13 +14,24 @@ class PostController extends Controller
 
     public function index()
     {
-        $posts = Post::with("user")->with("like")->orderBy("created_at", "desc")->paginate(3);
+        $posts = Post::with("user")->with("like")
+            ->leftJoin('follows', function ($join) {
+                $join->on('posts.user_id', '=', 'follows.following_id')->where('follows.follower_id', '=', Auth::user()->id);
+            })
+            ->where('posts.user_id', '=', Auth::user()->id)
+            ->orWhereNotNull('follows.follower_id')
+            ->select('posts.*')
+            ->orderBy('created_at', 'desc')->paginate(3);
 
         foreach ($posts as $key => $post) {
             $post->liked_by_user = false;
-            if ($post->like->contains('user_id', 1)) {
-               $post->liked_by_user = true;
+            if ($post->like->contains('user_id', Auth::user()->id)) {
+                $post->liked_by_user = true;
             }
+        }
+
+        foreach ($posts as $key => $post) {
+            unset($post->like);
         }
 
         return json_encode($posts);
